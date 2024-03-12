93d33f1710e95ab506eda3abbe4aeecfeb7832a8
omisonomoto0714 <159553436+omisonomoto0714@users.noreply.github.com>
2024-03-12 22:48:23 +0900

    kadai_020再提出用

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -7,7 +7,7 @@ const text = document.getElementById('text');
 
 //HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
-  text.textContent = 'ボタンをクリックしました'
+  text.textContent = 'ボタンをクリックしました';
   console.log('ボタンをクリックしました');
 });
 
